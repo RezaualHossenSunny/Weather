8b1c887c0ed43f8e3edfd57f19829d45ab95073f
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-10-14 19:42:47 +0600

    degain part done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,22 @@ function App() {
 
   return (
     <>
-   
+    <div className=" flex justify-center  items-center w-screen h-screen  ">
+      <div className="w-[500px] bg-slate-800 py-6 shadow-2xl p-5  rounded-md items-center flex flex-col">
+     <form >
+      <input className="w-[400px] px-2 py-3 rounded-md outline-none font-serif font-bold text-center"/>
+    </form>
+ 
+ <div className="text-center mt-4">
+ <p className="py-1 font-bold text-xl text-white ">Icon</p>
+    <h1 className="font-bold text-3xl text-white pb-1 ">32 &deg:C</h1>
+    <h2 className="font-bold text-2xl text-white pb-1 ">dhaka</h2>
+    <h3 className="font-bold text-1xl text-white pb-1 ">Last uypdae</h3>
+ </div>
+
+      </div>
+
+    </div>
     </>
   )
 }
